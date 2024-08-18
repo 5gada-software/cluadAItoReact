c88aade26d8903f49847cf0e8b4a895f945e2b54
Tokuma Abdisa <tokkiyuam@gmail.com>
2024-08-18 09:36:23 +0300

    update listing.jsx

--- frontend/src/components/listing.jsx
+++ frontend/src/components/listing.jsx
@@ -1,5 +1,19 @@
 import React from "react";
 
+const columns = ["Item", "Cubic Feet", "Quantity"];
+const inventoryData = [
+  { name: "AC, Large", cubicFeet: 20, category: "Appliances",quantity:0 },
+  { name: "Bed, Queen", cubicFeet: 65, category: "Bedroom",quantity:0 },
+  { name: "Pool Table", cubicFeet: 60, category: "Den",quantity:0 },
+  { name: "Table, Dining", cubicFeet: 30, category: "Dining Room" ,quantity:0},
+  { name: "Treadmill", cubicFeet: 25, category: "Exercise" ,quantity:0},
+  { name: "Entry Table", cubicFeet: 10, category: "Foyer" ,quantity:0},
+  { name: "Lawn Mower, Power", cubicFeet: 10, category: "Garage" ,quantity:0},
+  { name: "Desk, Executive", cubicFeet: 50, category: "Home Office" ,quantity:0},
+  { name: "Refrigerator, Small", cubicFeet: 20, category: "Kitchen",quantity:0 },
+  { name: "Sofa, 3 Cushion", cubicFeet: 50, category: "Living/Family Room",quantity:0 },
+];
+
 export default function Listing() {
   return <div></div>;
 }
