bed6d393eb134a969fb14991e40296be9304ef43
Tokuma Abdisa <tokkiyuam@gmail.com>
2024-08-18 11:15:25 +0300

    update listing.jsx

--- frontend/src/components/listing.jsx
+++ frontend/src/components/listing.jsx
@@ -180,7 +180,7 @@ export default function Listing({ filters }) {
           Items: {totals.items} | Cubic Feet: {totals.cubicFeet.toFixed(2)} |
           Weight: {totals.weight} lbs
         </div>
-        <button className="inline-flex items-center text-white justify-center whitespace-nowrap rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 bg-gray-900 text-primary-foreground shadow hover:bg-gray-900/90 h-9 px-4 py-2">
+        <button class="inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 bg-gray-900 text-white text-gray-100-foreground shadow hover:bg-gray-900/90 h-9 px-4 py-2">
           <svg
             xmlns="http://www.w3.org/2000/svg"
             width="20"
@@ -188,14 +188,16 @@ export default function Listing({ filters }) {
             viewBox="0 0 24 24"
             fill="none"
             stroke="currentColor"
-            strokeWidth="2"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            className="lucide lucide-plus-circle"
+            stroke-width="2"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            class="lucide lucide-save mr-1"
           >
-            <path d="M12 5v14M5 12h14"></path>
-          </svg>
-          <span className="ml-2">Add Item</span>
+            <path d="M15.2 3a2 2 0 0 1 1.4.6l3.8 3.8a2 2 0 0 1 .6 1.4V19a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2z"></path>
+            <path d="M17 21v-7a1 1 0 0 0-1-1H8a1 1 0 0 0-1 1v7"></path>
+            <path d="M7 3v4a1 1 0 0 0 1 1h7"></path>
+          </svg>{" "}
+          Save inventory
         </button>
       </div>
     </div>
