bb3e37b14c10e394683899784633193ab5c1ea57
Tokuma Abdisa <tokkiyuam@gmail.com>
2024-08-18 11:33:27 +0300

    update router.js

--- backend/routes/router.js
+++ backend/routes/router.js
@@ -2,9 +2,19 @@
 const express = require("express");
 const router = express.Router();
 
-// router.get(
-//   "/users",
-//   userController.getUsers
-// );
+const {
+  getAllInventories,
+  createInventory,
+  getInventoryById,
+  updateInventory,
+  deleteInventory,
+} = require("../controllers/inventoryController");
+
+// Routes
+router.get("/", getAllInventories);
+router.post("/", createInventory);
+router.get("/:id", getInventoryById);
+router.put("/:id", updateInventory);
+router.delete("/:id", deleteInventory);
 
 module.exports = router;
