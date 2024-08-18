7f2a55136c4042a265197314566dfaf58afe34da
Tokuma Abdisa <tokkiyuam@gmail.com>
2024-08-18 09:19:16 +0300

    update page1.jsx

--- frontend/src/pages/page1.jsx
+++ frontend/src/pages/page1.jsx
@@ -5,8 +5,10 @@ import AddCustomItemForm from "../components/AddCustomItemForm";
 export default function Page1() {
   return (
     <div className="w-[90%] sm:w-[80%] lg:w-[70%] mx-auto py-5">
-      <Search />
-      <AddCustomItemForm />
+      <div className="flex justify-between items-end">
+        <Search />
+        <AddCustomItemForm />
+      </div>
     </div>
   );
 }
