d26bb31b06b169f4cd0002a8f9b343c0cc5ab7df
Tokuma Abdisa <tokkiyuam@gmail.com>
2024-08-18 10:20:35 +0300

    update search.jsx

--- frontend/src/components/search.jsx
+++ frontend/src/components/search.jsx
@@ -7,7 +7,7 @@ export default function Search({ data }) {
       <h1 class="text-2xl font-bold mb-4">Select inventories</h1>
       <div class="flex items-center mb-4">
         <input
-          class="border-input placeholder:text-muted-foreground focus-visible:ring-ring flex h-9 w-full rounded-md border bg-transparent px-3 py-1 text-sm shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium focus-visible:outline-none focus-visible:ring-1 disabled:cursor-not-allowed disabled:opacity-50 mr-2"
+          class="border-input placeholder:text-muted-foreground focus-visible:ring-black flex h-9 w-full rounded-md border bg-transparent px-3 py-1 text-sm shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium focus-visible:outline-none focus-visible:ring-1 disabled:cursor-not-allowed disabled:opacity-50 mr-2"
           placeholder="Search inventories..."
           type="text"
           value=""
